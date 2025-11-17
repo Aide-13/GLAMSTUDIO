87c68a3c0ddaaa44b67d349433998b39d269c4d0
Aide-13 <223107072@cuautitlan.tecnm.mx>
2025-11-17 17:46:12 -0600

    manicura responsive

--- js/Manicura.js
+++ js/Manicura.js
@@ -15,3 +15,14 @@ function selectImage(element) {
   document.querySelectorAll('.circle img').forEach(img => img.classList.remove('selected'));
   element.classList.add('selected');
 }
+
+const botones = document.querySelectorAll('#grupoTamanos button');
+  const imagen = document.getElementById('imagenSeleccion');
+
+  botones.forEach(boton => {
+    boton.addEventListener('click', () => {
+      botones.forEach(b => b.classList.remove('active'));
+      boton.classList.add('active');
+      imagen.src = boton.getAttribute('data-img');
+    });
+  });
